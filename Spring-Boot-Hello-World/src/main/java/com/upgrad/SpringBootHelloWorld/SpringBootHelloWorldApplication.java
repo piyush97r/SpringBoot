ff31dbfd01d7877342ae4e10862c7e5295466abe
piyush97r <piyush97ranjan@gmail.com>
2021-01-04 19:18:26 +0530

    Spring Boot Example

---
+++ Spring-Boot-Hello-World/src/main/java/com/upgrad/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
@@ -0,0 +1,17 @@
+package com.upgrad.SpringBootHelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Run this java class and open the following link in your browser
+// URI : localhost:8080/hello
+
+
+@SpringBootApplication
+public class SpringBootHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-Hello-World/src/test/java/com/upgrad/SpringBootHelloWorld/SpringBootHelloWorldControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upgrad.SpringBootHelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloWorldControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
